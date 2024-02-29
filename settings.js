6bfe4b918be72bacc95979e5ec1ff809c26b83fe
drak1234 <161688340+drak1234@users.noreply.github.com>
2024-02-29 12:25:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "94775909265",
+  SUDO_NUMBERS : "94775909265,94775909265",
+  OWNER_NAME: "『 MR CHAMO』",
+  BOT_NAME : "Drack Chamo",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
